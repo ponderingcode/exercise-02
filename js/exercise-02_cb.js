3049dc8cb444639ef65ede09046316c33cb83f98
Ryan Fleming <ryan@ponderingcode.com>
2016-07-18 12:15:23 -0400

    changed result output from alert to text within a span, adjusted layout

--- js/exercise-02_cb.js
+++ js/exercise-02_cb.js
@@ -1,8 +1,13 @@
+$(document).ready(function() {});
+
 function onSubmitButtonPressed() {
+    event.preventDefault();
     var val0 = parseInt($('#inp0').val());
     var factorial = val0;
     for (i = 1; i < val0; i++) {
         factorial *= i;
     }
-    alert(val0 + ' factorial is ' + factorial);
+    var msg = val0 + ' factorial is ' + factorial;
+    $('#inp0').val('');
+    $('#output').text(msg);
 }
